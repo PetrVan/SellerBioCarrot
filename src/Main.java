9adfd19c7a728b1e9e0afa5fb737c01b2d68f0c0
PetrVan <vanicekpe@gmail.com>
2024-09-30 23:48:30 +0200

    Nastavení proměných a výpis jména a adresy prodejců

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Seller firstSeller = new Seller();
+        Seller secondSeller = new Seller();
+        firstSeller.setNameOfSeller("Pepa Novák");
+        firstSeller.setAddressOfSeller("Praha");
+        secondSeller.setNameOfSeller("Tomáš Hotoví");
+        secondSeller.setAddressOfSeller("Brno");
+
+        System.out.println("Prodejci mrkve: ");
+        System.out.println(firstSeller.getNameOfSeller());
+        System.out.println(secondSeller.getNameOfSeller());
     }
 }
--- src/Seller.java
+++ src/Seller.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Seller {
+    private LocalDate dateOfBirth;
+    private String nameOfSeller;
+    private int numberOfContracts;
+    private int quantityOfCarrots;
+    private String AddressOfSeller;
+    private String registrationNumber;
+
+    public String getNameOfSeller() {
+        return nameOfSeller;
+    }
+
+    public void setNameOfSeller(String nameOfSeller) {
+        this.nameOfSeller = nameOfSeller;
+    }
+
+    public String getAddressOfSeller() {
+        return AddressOfSeller;
+    }
+
+    public void setAddressOfSeller(String addressOfSeller) {
+        AddressOfSeller = addressOfSeller;
+    }
+
+    private double carConsumption;
 }
